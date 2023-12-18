cc582e85f0c7751d18a809822913b93a9bc23073
ThiagoAguirre <thiago.aguirre08@outlook.com>
2023-12-18 18:00:56 -0300

    Project changes

--- src/js/index.js
+++ src/js/index.js
@@ -3,16 +3,27 @@ function logar(){
     var login = document.getElementById('login').value;
     var senha = document.getElementById('senha').value;
 
-    if(login == "123" && senha == "123"){
-        alert(`Bem vindo ${login} a ORDEM`);
-        location.href = "./src/cesar/index.html";
-    }else if(login == "222" && senha == "222"){
+    if(login == "demonio" && senha == "666"){
+        alert(`Bem vindo Lucy F. Gulevart a ORDEM`);
+        location.href = "./src/caracters/misth/index.html";
+    }
+    else if(login == "hunter" && senha == "242"){
+        alert(`Bem vindo K a ORDEM`)
+        location.href = "./src/caracters/cesar/index.html"
+    }
+    else if(login == "covarde" && senha == "vida"){
+        alert(`Bem vindo Samuel Nakamura a ORDEM`)
+        location.href = "./src/caracters/Xuxu/index.html"
+    }
+    else if(login == "lixo" && senha == "morte"){
+        alert(`Bem vindo Yuri Nakamura a ORDEM`)
+        location.href = "./src/caracters/rafa/index.html"
+    }
+    else if(login == "arrozdamamae" && senha == "9y4s2da7"){
         alert(`Bem vindo ${login} a ORDEM`)
-        location.href = "./src/matrix/home.html"
-    }else if(login == "arrozdamamae" && senha == "9y4s2da7"){
-        alert(`Bem vindo ${login} a ORDEM`)
-        location.href = "./src/arroz/index.html";
-    }else{
+        location.href = "./src/caracters/arroz/index.html";
+    }
+    else{
         alert('Usuario ou senha incorretos');
     }
 
